a7822531ee249337fc03675ee0ad87b44d09a481
simod <simone.dalmasso@gmail.com>
2011-12-14 14:29:36 +0100

    edited setup.py

--- setup.py
+++ setup.py
@@ -2,29 +2,29 @@ import os
 from setuptools import setup, find_packages
 
 def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+	return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(
-    name="geonode-documents",
-    version="0.1",
-    author="Simone Dalmasso, Ariel Nunez",
-    author_email="simone.dalmasso@gmail.com",
-    description="Documents extension for GeoNode",
-    long_description=(read('README.rst')),
-    classifiers=[
-        'Development Status :: 2 - Pre-alpha',
-        'Framework :: GeoNode',
-        'License :: OSI Approved :: BSD',
-    ],
-    license="BSD",
-    keywords="geonode django",
-    url='https://github.com/simod/geonode-documents',
-    scripts = [
-              ],
-    packages=find_packages('.'),
-    include_package_data=True,
+	name="geonode-documents",
+	version="0.1",
+	author="Simone Dalmasso, Ariel Nunez",
+	author_email="simone.dalmasso@gmail.com",
+	description="Documents extension for GeoNode",
+	long_description=(read('README.rst')),
+	classifiers=[
+		'Development Status :: 2 - Pre-alpha',
+		'Framework :: GeoNode',
+		'License :: OSI Approved :: BSD',
+	],
+	license="BSD",
+	keywords="geonode django",
+	url='https://github.com/simod/geonode-documents',
+	scripts = [
+			  ],
+	packages=find_packages('.'),
+	include_package_data=True,
 	package_data={
-		'':['fixtures/*.*','templates/*.*'],
+		'':['fixtures/*.*'],
 	},
-    zip_safe=False,
+	zip_safe=False,
 )
